44a5db6642a33fb90e6ddff521c5d2315241eb88
naspara <94629851+naspara@users.noreply.github.com>
2022-04-12 02:45:07 +0300

    recipe widget

---
+++ lib/widgets/recipe_card.dart
@@ -0,0 +1,40 @@
+
+
+import 'package:flutter/material.dart';
+
+class RecipeCard extends StatelessWidget {
+  final String? title;
+  final String? imgURL;//url for now
+  final double? height;
+  final double? width;
+
+  const RecipeCard(
+      this.title,
+      this.imgURL,
+      this.height,
+      this.width,
+      );
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){} ,// will open the detailed recipe page
+      borderRadius: BorderRadius.circular(20),
+      child: Column(
+        children: [
+          const Text(
+            "Pizza"
+          ),
+          Ink.image(
+            fit: BoxFit.cover,
+            width: 400,
+            height: 100,
+            image: const NetworkImage("https://cdn.yemek.com/mncrop/600/315/uploads/2017/01/ev-usulu-pizza-yeni.jpg"),
+          ),
+        ],
+      ),
+
+    );
+  }}

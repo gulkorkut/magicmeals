6d7c5829e3c1005518731de465c73f197613471d
meredax <mertfberdem@gmail.com>
2022-04-18 16:05:06 +0300

    Rating bar added

--- lib/pages/recipe_detailed_page.dart
+++ lib/pages/recipe_detailed_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class RecipeDetailed extends StatefulWidget {
   const RecipeDetailed({Key? key}) : super(key: key);
@@ -11,10 +12,70 @@ class RecipeDetailed extends StatefulWidget {
 
 class _RecipeDetailedState extends State<RecipeDetailed> {
   @override
+  double? _ratingValue;
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text('just want to see rating'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  'Rate',
+                  style: TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 25),
+                // implement the rating bar
+                Transform.scale(
+                  scale: 1,
+                  child: RatingBar(
+                      initialRating: 0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      ratingWidget: RatingWidget(
+                          full: const Icon(Icons.star,
+                              size: 15,
+                              color: Colors.black),
+                          half: const Icon(
+                            Icons.star_half,
+                            size: 15,
+                            color: Colors.black,
+                          ),
+                          empty: const Icon(
+                            Icons.star_outline,
+                            size: 15,
+                            color: Colors.black,
+                          )),
+                      onRatingUpdate: (value) {
+                        setState(() {
+                          _ratingValue = value;
+                        });
+                      }),
+                ),
+                const SizedBox(height: 25),
+                // Display the rate in number
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      color: Colors.black, shape: BoxShape.circle),
+                  alignment: Alignment.center,
+                  child: Text(
+                    _ratingValue != null ? _ratingValue.toString() : 'Rate it!',
+                    style: const TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
 
-    );
+
+
+
   }
 }

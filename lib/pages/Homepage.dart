53f9d4da887e8512d5320e3e7511b3aa82c83424
ibugra <tutanc.bugra@gmail.com>
2022-04-20 15:52:40 +0300

    SingleChildScrollView in Recipe Detailed Page.

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:magicmeals202/widgets/recipe_card.dart';
--- lib/pages/recipe_detailed_page.dart
+++ lib/pages/recipe_detailed_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -22,63 +21,65 @@ class _RecipeDetailedState extends State<RecipeDetailed> {
         appBar: AppBar(
           title: Text(recipe.title),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(25),
-          child: Center(
-            child: Column(
-              children: [
-                ClipRRect(
-                  child: Image.network(recipe.imageUrl),
-                ),
-                const SizedBox(height: 15),
-                // description,preparation etc will be here
-                const Text(
-                  'Rate',
-                  style: TextStyle(fontSize: 24),
-                ),
-                const SizedBox(height: 25),
-                // implement the rating bar
-                Transform.scale(
-                  scale: 1,
-                  child: RatingBar(
-                      initialRating: 0,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      ratingWidget: RatingWidget(
-                          full: const Icon(Icons.star,
-                              size: 15,
-                              color: Colors.black),
-                          half: const Icon(
-                            Icons.star_half,
-                            size: 15,
-                            color: Colors.black,
-                          ),
-                          empty: const Icon(
-                            Icons.star_outline,
-                            size: 15,
-                            color: Colors.black,
-                          )),
-                      onRatingUpdate: (value) {
-                        setState(() {
-                          _ratingValue = value;
-                        });
-                      }),
-                ),
-                const SizedBox(height: 25),
-                // Display the rate in number
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: const BoxDecoration(
-                      color: Colors.black, shape: BoxShape.circle),
-                  alignment: Alignment.center,
-                  child: Text(
-                    _ratingValue != null ? _ratingValue.toString() : 'Rate it!',
-                    style: const TextStyle(color: Colors.white, fontSize: 30),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Center(
+              child: Column(
+                children: [
+                  ClipRRect(
+                    child: Image.network(recipe.imageUrl),
                   ),
-                )
-              ],
+                  const SizedBox(height: 15),
+                  // description,preparation etc will be here
+                  const Text(
+                    'Rate',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 25),
+                  // implement the rating bar
+                  Transform.scale(
+                    scale: 1,
+                    child: RatingBar(
+                        initialRating: 0,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        ratingWidget: RatingWidget(
+                            full: const Icon(Icons.star,
+                                size: 15,
+                                color: Colors.black),
+                            half: const Icon(
+                              Icons.star_half,
+                              size: 15,
+                              color: Colors.black,
+                            ),
+                            empty: const Icon(
+                              Icons.star_outline,
+                              size: 15,
+                              color: Colors.black,
+                            )),
+                        onRatingUpdate: (value) {
+                          setState(() {
+                            _ratingValue = value;
+                          });
+                        }),
+                  ),
+                  const SizedBox(height: 25),
+                  // Display the rate in number
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                        color: Colors.black, shape: BoxShape.circle),
+                    alignment: Alignment.center,
+                    child: Text(
+                      _ratingValue != null ? _ratingValue.toString() : 'Rate it!',
+                      style: const TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));

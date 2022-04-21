7019b7d43d67be16f72d3da87efd066aefa62832
gulkorkut <serifegulkorkut@gmail.com>
2022-04-22 01:12:36 +0300

    comment page added

--- lib/pages/comments_page.dart
+++ lib/pages/comments_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:comment_box/comment/comment.dart';
 
 class CommentPage extends StatefulWidget {
   const CommentPage({Key? key}) : super(key: key);
@@ -9,40 +10,85 @@ class CommentPage extends StatefulWidget {
 }
 
 class _CommentPageState extends State<CommentPage> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController commentController = TextEditingController();
+  List filedata = [
+
+  ];
+
+  Widget commentChild(data) {
+    return ListView(
+      children: [
+        for (var i = 0; i < data.length; i++)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 0.0),
+            child: ListTile(
+              leading: GestureDetector(
+                onTap: () async {
+                  // Display the image in large form.
+                  print("Comment Clicked");
+                },
+                child: Container(
+                  height: 50.0,
+                  width: 50.0,
+                  decoration: new BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: new BorderRadius.all(Radius.circular(50))),
+                  child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(data[i]['pic'] + "$i")),
+                ),
+              ),
+              title: Text(
+                data[i]['name'],
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(data[i]['message']),
+            ),
+          )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController commentCont = TextEditingController();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Comments"),
+        title: Text("Comment Page"),
+        backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.separated(
-                itemBuilder: (BuildContext context,int index){
-                  return ListTile(
-                    title: Text("Default Comment"),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                const Divider(),
-                itemCount: 4),
-          ),
-          SizedBox(height: 50,),
-          ListTile(
-            title: TextFormField(
-              controller: commentCont,
-              decoration: InputDecoration(labelText: "Write a comment"),
-            ),
-            trailing: OutlinedButton(
-                onPressed: (){
-
-                },
-                child: const Text("Save comment")),
-          )
-
-        ],
+      body: Container(
+        child: CommentBox(
+          userImage:
+          "https://lh3.googleusercontent.com/a-/AOh14GjRHcaendrf6gU5fPIVd8GIl1OgblrMMvGUoCBj4g=s400",
+          child: commentChild(filedata),
+          labelText: 'Write a comment...',
+          withBorder: false,
+          errorText: 'Comment cannot be blank',
+          sendButtonMethod: () {
+            if (formKey.currentState!.validate()) {
+              print(commentController.text);
+              setState(() {
+                var value = {
+                  'name': 'New User',
+                  'pic':
+                  'https://lh3.googleusercontent.com/a-/AOh14GjRHcaendrf6gU5fPIVd8GIl1OgblrMMvGUoCBj4g=s400',
+                  'message': commentController.text
+                };
+                filedata.insert(0, value);
+              });
+              commentController.clear();
+              FocusScope.of(context).unfocus();
+            } else {
+              print("Not validated");
+            }
+          },
+          formKey: formKey,
+          commentController: commentController,
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+          sendWidget: Icon(Icons.send_sharp, size: 30, color: Colors.white),
+        ),
       ),
     );
   }
